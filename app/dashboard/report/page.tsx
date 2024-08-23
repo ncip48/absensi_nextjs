863ee8a36144d1062520f48040a503be84225d07
ncip48 <mbahcip00@gmail.com>
2024-08-24 00:32:21 +0700

    rev: filter

--- app/dashboard/report/page.tsx
+++ app/dashboard/report/page.tsx
@@ -5,15 +5,26 @@ import useEffectAfterMount from "@/utils/useEffectAfterMount";
 import React, { useState } from "react";
 import DashboardNavbar from "../_components/DashboardNavbar";
 import CardMain from "@/components/CardMain";
-import { getAttendances } from "@/services/actions/report";
+import { getAttendanceRange } from "@/services/actions/report";
 
 function Index() {
   const [students, setStudents] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [dateStart, setDateStart] = useState(
+    new Date(new Date().setDate(new Date().getDate() - 7))
+      .toISOString()
+      .split("T")[0]
+  );
+  const [dateEnd, setDateEnd] = useState(
+    new Date().toISOString().split("T")[0]
+  );
 
   const getData = async () => {
     setLoading(true);
-    let res = await getAttendances();
+    let res = await getAttendanceRange(
+      dateStart + "T00:00:00",
+      dateEnd + "T00:00:00"
+    );
     res?.map((item: any) => {
       item.timein_parse = new Date(item.timein).toLocaleTimeString([], {
         hour: "2-digit",
@@ -21,6 +32,7 @@ function Index() {
         second: "2-digit",
         hour12: false,
       });
+      item.tanggal = new Date(item.timein).toLocaleDateString();
     });
     // console.table(res);
     setStudents(res);
@@ -29,17 +41,43 @@ function Index() {
 
   useEffectAfterMount(() => {
     getData();
-  }, []);
+  }, [dateStart, dateEnd]);
 
   return (
     <>
       <DashboardNavbar active="Report" />
       <CardMain title="Laporan Absensi">
+        <div className="flex flex-row gap-2 mb-6">
+          <div className="flex items-center">
+            <input
+              type="date"
+              id="dateStart"
+              value={dateStart}
+              onChange={(e) => setDateStart(e.target.value)}
+              className="px-3 py-2.5 ml-6 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm dark:bg-dark-900 dark:text-gray-300"
+            />
+          </div>
+          <div className="flex items-center">
+            <label
+              htmlFor="dateEnd"
+              className="text-sm font-medium text-gray-700 dark:text-gray-400"
+            >
+              Sampai
+            </label>
+            <input
+              type="date"
+              id="dateEnd"
+              value={dateEnd}
+              onChange={(e) => setDateEnd(e.target.value)}
+              className="px-3 py-2.5 ml-2 rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm dark:bg-dark-900 dark:text-gray-300"
+            />
+          </div>
+        </div>
         <Table
           items={students}
           loading={loading}
-          heads={["NIS", "Nama", "Jam Masuk"]}
-          keys={["student.nis", "student.name", "timein_parse"]}
+          heads={["Tanggal", "NIS", "Nama", "Jam Masuk"]}
+          keys={["tanggal", "student.nis", "student.name", "timein_parse"]}
           noAction
           noStatus
         />
--- services/actions/report.tsx
+++ services/actions/report.tsx
@@ -42,3 +42,72 @@ export async function getAttendances() {
     return;
   }
 }
+
+export async function getAttendanceToday() {
+  try {
+    const storage = await getSession();
+    const token = storage.user.token;
+    const response = await axios.get(`${baseUrl}/attendances/v1/today`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = response.data;
+    if (result.status !== 200) {
+      toast.error(result.message);
+      return [];
+    } else {
+      return result.data;
+    }
+  } catch (error: any) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      if (error.response.status === 401) {
+        // await logout();
+        toast.error(error.message);
+        return;
+      }
+    }
+    toast.error(error.message);
+    return;
+  }
+}
+
+export async function getAttendanceRange(dateStart: string, dateEnd: string) {
+  try {
+    const storage = await getSession();
+    const token = storage.user.token;
+    const response = await axios.get(
+      `${baseUrl}/attendances/v1/range?start=${dateStart}&end=${dateEnd}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const result = response.data;
+    if (result.status !== 200) {
+      toast.error(result.message);
+      return [];
+    } else {
+      return result.data;
+    }
+  } catch (error: any) {
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      if (error.response.status === 401) {
+        // await logout();
+        toast.error(error.message);
+        return;
+      }
+    }
+    toast.error(error.message);
+    return;
+  }
+}
